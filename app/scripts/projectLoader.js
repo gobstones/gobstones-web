7f5fbb7b3972249558a4dde2e065e1284e7c3d9e
Rodrigo Alfonso <rodri042@gmail.com>
2016-10-14 02:44:50 -0300

    Moved logic to ProjectLoader

---
+++ app/scripts/projectLoader.js
@@ -0,0 +1,22 @@
+class ProjectLoader {
+  save(content, name) {
+    const a = document.createElement("a");
+    const file = new Blob([content], { type: "text/plain" });
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+    a.click();
+  }
+
+  read(event, callback) {
+    const input = event.target;
+
+    const fileName = input.files[0].name.split(".")[0];
+
+    const reader = new FileReader();
+    reader.onload = function(){
+      const content = reader.result;
+      callback(content, fileName);
+    };
+    reader.readAsText(input.files[0]);
+  }
+}
