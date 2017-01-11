b370ac3e61b1dee6d38ad299aee951823ebd1337
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-11 16:33:33 -0300

    Add teacherLoader

--- app/scripts/loaders/projectLoader.js
+++ app/scripts/loaders/projectLoader.js
@@ -5,6 +5,7 @@ class ProjectLoader extends Loader {
     this.loaders = [
       new CodeLoader,
       new LibraryLoader,
+      new TeacherLoader,
       new InitialBoardLoader,
       new AttireLoader
     ];
