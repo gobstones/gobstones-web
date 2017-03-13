a9cf45d9b08ae5d2152ca4522941c175bced8e69
Rodrigo Alfonso <rodri042@gmail.com>
2017-03-13 20:42:05 -0300

    Improved start-desktop script

--- start-desktop.sh
+++ start-desktop.sh
@@ -5,10 +5,25 @@ if ! which python >/dev/null; then
   exit 1
 fi
 
+browser=google-chrome
 if ! which 'google-chrome' > /dev/null; then
-  echo 'The command "google-chrome" is required.'
-  exit 1
+  browser=firefox
+  if ! which 'firefox' > /dev/null; then
+    echo 'The commands "google-chrome" or "firefox" are required.'
+    exit 1
+  fi
 fi
 
-python -m SimpleHTTPServer 6085 &
-google-chrome --app=http://localhost:6085
+BASE=3000
+INCREMENT=1
+
+port=$BASE
+isfree=$(netstat -tapln | grep $port)
+
+while [[ -n "$isfree" ]]; do
+  port=$[port+INCREMENT]
+  isfree=$(netstat -tapln | grep $port)
+done
+
+sleep 1 && eval "$browser http://localhost:$port" &
+python -m SimpleHTTPServer $port
