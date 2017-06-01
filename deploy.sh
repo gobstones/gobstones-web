27380734f0a9f9b44de2a3306bac6efdd4a17558
Rodrigo Alfonso <rodri042@gmail.com>
2017-06-01 17:00:27 -0300

    Trying to debug deploy script on travis

--- deploy.sh
+++ deploy.sh
@@ -16,20 +16,28 @@ try gulp
 # config
 ENVIRONMENT="$1"
 SUFFIX="-staging"
+REMOTE="staging"
 if [ "$ENVIRONMENT" == "production" ]; then
   SUFFIX=""
+  REMOTE="origin"
 fi
 
+echo "Deploying to gobstones-web$SUFFIX (remote $REMOTE)..."
+
 # push to gh pages
-current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+#current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+echo "Deleting temporary branches..."
 git branch -D deploy
 git branch -D tmp-deploy
+echo "Adding remote..."
 git remote remove staging
 git remote add staging https://$GH_TOKEN@github.com/gobstones/gobstones-web$SUFFIX
-echo "Deploying to environment '$ENVIRONMENT'..."
+echo "Creating deploy commit..."
 try git checkout -b deploy
 try git add -Af dist/
 try git commit -m "Deploy @ $(date +'%d/%m/%Y')"
+echo "Creating deploy subtree commit..."
 try git subtree split --prefix dist deploy -b tmp-deploy
-try git push -f staging tmp-deploy:gh-pages
-try git checkout "$current_branch"
+eho "Pushing to remote..."
+try git push -f $REMOTE tmp-deploy:gh-pages
+#try git checkout "$current_branch"
