c6e79feba08f83175addb03943af07296756d089
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-24 01:05:06 -0300

    Updating desktop deploy script with new names and adding desktop release

--- release-desktop.sh
+++ release-desktop.sh
@@ -22,6 +22,20 @@ sed -i -e "s/'\.'/'\.\/resources\/app'/g" start-electron.js
 mkdir build
 mv icon.png build/
 
+function commertialName() {
+  if [ "$1" == "blocks" ] ; then
+    echo "jr"
+  fi
+
+  if [ "$1" == "code" ] ; then
+    echo "sr"
+  fi
+
+  if [ "$1" == "teacher" ] ; then
+    echo "teacher"
+  fi
+}
+
 # ---
 
 TYPE="blocks"
@@ -30,15 +44,14 @@ echo "BUILDING '$TYPE' WITH ELECTRON..."
 ./node_modules/.bin/electron-builder . gobstones-web -wl
 
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
-LINUX_NAME_1=gobstones-$TYPE-linux-$PACKAGE_VERSION.zip
-WINDOWS_NAME_1=gobstones-$TYPE-windows-$PACKAGE_VERSION.exe
+LINUX_NAME_1=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
+WINDOWS_NAME_1=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_1 . ; cd ../..
 cp "./dist/gobstones-web Setup 1.0.0.exe" $WINDOWS_NAME_1
-
-# ---
-
 mv "$LINUX_NAME_1" node_modules/
 mv "$WINDOWS_NAME_1" node_modules/
+
+# ---
 
 TYPE="code"
 sed -i -e "s/\/#\/blocks/\/#\/code/g" start-electron.js
@@ -47,13 +60,37 @@ echo "BUILDING '$TYPE' WITH ELECTRON..."
 ./node_modules/.bin/electron-builder . gobstones-web -wl
 
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
-LINUX_NAME_2=gobstones-$TYPE-linux-$PACKAGE_VERSION.zip
-WINDOWS_NAME_2=gobstones-$TYPE-windows-$PACKAGE_VERSION.exe
+LINUX_NAME_2=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
+WINDOWS_NAME_2=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_2 . ; cd ../..
 cp "./dist/gobstones-web Setup 1.0.0.exe" $WINDOWS_NAME_2
+mv "$LINUX_NAME_2" node_modules/
+mv "$WINDOWS_NAME_2" node_modules/
+
+# ---
+
+TYPE="teacher"
+sed -i -e "s/\/#\/code/\/#\/teacher/g" start-electron.js
+
+echo "BUILDING '$TYPE' WITH ELECTRON..."
+./node_modules/.bin/electron-builder . gobstones-web -wl
+
+echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
+LINUX_NAME_3=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
+WINDOWS_NAME_3=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
+cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_3 . ; cd ../..
+cp "./dist/gobstones-web Setup 1.0.0.exe" $WINDOWS_NAME_3
+mv "$LINUX_NAME_3" node_modules/
+mv "$WINDOWS_NAME_3" node_modules/
+
+# ---
 
 mv "node_modules/$LINUX_NAME_1" "$LINUX_NAME_1"
 mv "node_modules/$WINDOWS_NAME_1" "$WINDOWS_NAME_1"
+mv "node_modules/$LINUX_NAME_2" "$LINUX_NAME_2"
+mv "node_modules/$WINDOWS_NAME_2" "$WINDOWS_NAME_2"
+mv "node_modules/$LINUX_NAME_3" "$LINUX_NAME_3"
+mv "node_modules/$WINDOWS_NAME_3" "$WINDOWS_NAME_3"
 
 # ---
 
@@ -63,7 +100,7 @@ echo "GIVE ME THE GITHUB TOKEN"
 read TOKEN
 
 echo "PUBLISHING..."
-./node_modules/.bin/publish-release --token $TOKEN --owner gobstones --repo gobstones-web-desktop --tag "$PACKAGE_VERSION" --name "$PACKAGE_VERSION" --assets $LINUX_NAME_1,$WINDOWS_NAME_1,$LINUX_NAME_2,$WINDOWS_NAME_2 --notes "Gobstones Web - Desktop"
+./node_modules/.bin/publish-release --token $TOKEN --owner gobstones --repo gobstones-web-desktop --tag "$PACKAGE_VERSION" --name "$PACKAGE_VERSION" --assets $LINUX_NAME_1,$WINDOWS_NAME_1,$LINUX_NAME_2,$WINDOWS_NAME_2,$LINUX_NAME_3,$WINDOWS_NAME_3 --notes "Gobstones Web - Desktop"
 
 echo "DONE."
 
