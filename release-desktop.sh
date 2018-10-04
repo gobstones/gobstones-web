2c2aef353bbd43939e2555b04a37e00a6217b1bf
Rodrigo Alfonso <rodri042@gmail.com>
2018-10-04 02:47:29 -0300

    Updating release script

--- release-desktop.sh
+++ release-desktop.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-echo "REMEMBER TO USE NODE 7 (nvm use 7) - ¿ARE YOU USING IT?"
+echo "REMEMBER TO USE NODE 7 (nvm use >=7) - ¿ARE YOU USING IT?"
 read _NOTHING_
 
-git pull
-
 PACKAGE_VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
-
-cp package.json node_modules/
-cp release-desktop.sh node_modules/
-cp icon.png node_modules/
-git branch -D electron
-git checkout origin/gh-pages
-git checkout -b electron
-cp node_modules/package.json package.json
-cp node_modules/release-desktop.sh release-desktop.sh
-cp node_modules/icon.png icon.png
-
-npm install
-sed -i -e "s/'\.'/'\.\/resources\/app'/g" start-electron.js
-mkdir build
-mv icon.png build/
 
 function commertialName() {
   if [ "$1" == "blocks" ] ; then
@@ -36,14 +19,18 @@ function commertialName() {
   fi
 }
 
+function build() {
+  rm -rf ./dist/
+  yarn dist --mode "$TYPE" --platform linux --arch x64 --target AppImage
+  yarn dist --mode "$TYPE" --platform win --arch ia32 --target nsis
+  yarn dist --mode "$TYPE" --platform win --arch x64 --target nsis
+}
+
 # ---
 
 TYPE="blocks"
-sed -i -e "s/-web/-jr/g" package.json
-sed -i -e "s/Gobstones Web/Gobstones Jr/g" package.json
-
 echo "BUILDING '$TYPE' WITH ELECTRON..."
-./node_modules/.bin/electron-builder . gobstones-$(commertialName $TYPE) -wl --x64 --ia32
+build
 
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_1=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
@@ -51,7 +38,7 @@ WINDOWS_NAME_1=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 WINDOWS32_NAME_1=gobstones-$(commertialName $TYPE)-windows-ia32-$PACKAGE_VERSION.zip
 cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_1 . ; cd ../..
 cd ./dist/win-ia32-unpacked ; zip -r ../../$WINDOWS32_NAME_1 . ; cd ../..
-cp "./dist/gobstones-$(commertialName $TYPE) Setup 1.0.0.exe" $WINDOWS_NAME_1
+cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_1
 mv "$LINUX_NAME_1" node_modules/
 mv "$WINDOWS_NAME_1" node_modules/
 mv "$WINDOWS32_NAME_1" node_modules/
@@ -59,36 +46,28 @@ mv "$WINDOWS32_NAME_1" node_modules/
 # ---
 
 TYPE="code"
-sed -i -e "s/\/#\/blocks/\/#\/code/g" start-electron.js
-sed -i -e "s/-jr/-sr/g" package.json
-sed -i -e "s/Gobstones Jr/Gobstones Sr/g" package.json
-
 echo "BUILDING '$TYPE' WITH ELECTRON..."
-./node_modules/.bin/electron-builder . gobstones-$(commertialName $TYPE) -wl
+build
 
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_2=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
 WINDOWS_NAME_2=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_2 . ; cd ../..
-cp "./dist/gobstones-$(commertialName $TYPE) Setup 1.0.0.exe" $WINDOWS_NAME_2
+cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_2
 mv "$LINUX_NAME_2" node_modules/
 mv "$WINDOWS_NAME_2" node_modules/
 
 # ---
 
 TYPE="teacher"
-sed -i -e "s/\/#\/code/\/#\/teacher/g" start-electron.js
-sed -i -e "s/-sr/-teacher/g" package.json
-sed -i -e "s/Gobstones Sr/Gobstones Teacher/g" package.json
-
 echo "BUILDING '$TYPE' WITH ELECTRON..."
-./node_modules/.bin/electron-builder . gobstones-$(commertialName $TYPE) -wl
+build
 
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_3=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
 WINDOWS_NAME_3=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_3 . ; cd ../..
-cp "./dist/gobstones-$(commertialName $TYPE) Setup 1.0.0.exe" $WINDOWS_NAME_3
+cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_3
 mv "$LINUX_NAME_3" node_modules/
 mv "$WINDOWS_NAME_3" node_modules/
 
