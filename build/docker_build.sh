998f05ece00e02287135f31129b9135449414ad7
Rodrigo Alfonso <rodri042@gmail.com>
2018-10-05 04:34:33 -0300

    Updating release-desktop to work with docker so Travis can automatically build desktop versions

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-docker run --rm -ti \
+
+INTERACTIVE="i"
+RUN="/bin/bash"
+COMMAND="$1"
+
+if [ -n "$COMMAND" ]; then
+    INTERACTIVE=""
+    RUN="$COMMAND"
+fi
+
+docker run --rm -t$INTERACTIVE \
     --env ELECTRON_CACHE="/root/.cache/electron" \
     --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
     -v ${PWD}:/project \
-    -v ${PWD##*/}-node-modules:/project/node_modules \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/builder:wine \
-#    /bin/bash -c "chmod +x ./node_modules/7zip-bin/compress.sh && yarn && yarn dist";
+    /bin/bash -c "$RUN"
