58d0dd8efbc48ed519aa719da4685b2da043aea3
Rodrigo Alfonso <rodri042@gmail.com>
2018-10-05 13:31:55 -0300

    Using zip's quiet mode

--- release-desktop.sh
+++ release-desktop.sh
@@ -53,8 +53,8 @@ echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_1=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
 WINDOWS_NAME_1=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
 WINDOWS32_NAME_1=gobstones-$(commertialName $TYPE)-windows-ia32-$PACKAGE_VERSION.zip
-cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_1 . ; cd ../..
-cd ./dist/win-ia32-unpacked ; zip -r ../../$WINDOWS32_NAME_1 . ; cd ../..
+cd ./dist/linux-unpacked ; zip -rq ../../$LINUX_NAME_1 . ; cd ../..
+cd ./dist/win-ia32-unpacked ; zip -rq ../../$WINDOWS32_NAME_1 . ; cd ../..
 cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_1
 mv "$LINUX_NAME_1" node_modules/
 mv "$WINDOWS_NAME_1" node_modules/
@@ -69,7 +69,7 @@ build
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_2=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
 WINDOWS_NAME_2=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
-cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_2 . ; cd ../..
+cd ./dist/linux-unpacked ; zip -rq ../../$LINUX_NAME_2 . ; cd ../..
 cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_2
 mv "$LINUX_NAME_2" node_modules/
 mv "$WINDOWS_NAME_2" node_modules/
@@ -83,7 +83,7 @@ build
 echo "CREATING '$TYPE' ONE-FILE PACKAGES..."
 LINUX_NAME_3=gobstones-$(commertialName $TYPE)-linux-$PACKAGE_VERSION.zip
 WINDOWS_NAME_3=gobstones-$(commertialName $TYPE)-windows-$PACKAGE_VERSION.exe
-cd ./dist/linux-unpacked ; zip -r ../../$LINUX_NAME_3 . ; cd ../..
+cd ./dist/linux-unpacked ; zip -rq ../../$LINUX_NAME_3 . ; cd ../..
 cp "./dist/gobstones-$(commertialName $TYPE)_$(echo $PACKAGE_VERSION)_win.exe" $WINDOWS_NAME_3
 mv "$LINUX_NAME_3" node_modules/
 mv "$WINDOWS_NAME_3" node_modules/
