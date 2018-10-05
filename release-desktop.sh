d73e3653e69948b3a025170a4a27cd6721b65315
Rodrigo Alfonso <rodri042@gmail.com>
2018-10-05 04:38:09 -0300

    Updating release-desktop to work with docker so Travis can automatically build desktop versions

--- release-desktop.sh
+++ release-desktop.sh
@@ -38,9 +38,9 @@ function build() {
   unset GH_TOKEN
   rm -rf ./dist/
 
-  try yarn dist --mode "$TYPE" --platform linux --arch x64 --target AppImage
-  try yarn dist --mode "$TYPE" --platform win --arch ia32 --target nsis
-  try yarn dist --mode "$TYPE" --platform win --arch x64 --target nsis
+  try yarn dist:docker "yarn dist --mode $TYPE --platform linux --arch x64 --target AppImage"
+  try yarn dist:docker "yarn dist --mode $TYPE --platform win --arch ia32 --target nsis"
+  try yarn dist:docker "yarn dist --mode $TYPE --platform win --arch x64 --target nsis"
 }
 
 # ---
